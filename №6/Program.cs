14063171804bb200deb77b4e2f0af7de9275c4ae
GRMarkVL <ClimatFL@gmail.com>
2022-08-06 20:54:14 +0300

    решил задачу №6

---
+++ №6/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая на вход принимает
+// число и выдаёт, является ли число чётным
+
+int a;
+
+ Console.WriteLine("Введите число : ");
+
+a = Convert.ToInt32(Console.ReadLine());
+      {
+        if (a % 2 == 0) Console.WriteLine("число четное");
+          else Console.WriteLine("число нечетное");
+      }
+      
