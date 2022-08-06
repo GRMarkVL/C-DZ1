3daaa4e6088c29fbc15943d825c664acd5ca1626
GRMarkVL <ClimatFL@gmail.com>
2022-08-06 19:20:37 +0300

    решил задачу № 4

--- l№4/Program.cs
+++ l№4/Program.cs
@@ -1,11 +1,20 @@
 ﻿//Напишите програму , которая принимает на вход
 // три числа и выдает максимальное из этих чисел
-Console.Write("a: ");
-var a = int.Parse(Console.ReadLine());
-Console.Write("b: ");
-var b = int.Parse(Console.ReadLine());
-Console.Write("c: ");
-var c = int.Parse(Console.ReadLine());
-Console.WriteLine((a > b) ? (a > c) ? a : c : (b > c) ? b : c );
-Console.ReadKey();
 
+int a, b, c;
+
+ Console.WriteLine("Введите два числа : ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+b = Convert.ToInt32(Console.ReadLine());
+
+c = Convert.ToInt32(Console.ReadLine());
+    {
+        int max=a;
+        if (a > b) max=a;
+            if (b > max) max=b;
+                if(c > max) max=c;
+                    if ((max==a)&&(max==b)&&(max==c)) Console.WriteLine("Все три числа равны!");
+                        else Console.WriteLine($"Максимальное число из трех чисел: {max}");
+    }                    
