bcf36893b61c8f8bdc81f4676ca3d23f2035d2eb
GRMarkVL <ClimatFL@gmail.com>
2022-08-06 18:01:05 +0300

    написали программу для решение задачи №2

---
+++ l№4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите програму , которая принимает на вход
+// три числа и выдает максимальное из этих чисел
+Console.Write("a: ");
+var a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+var b = int.Parse(Console.ReadLine());
+Console.Write("c: ");
+var c = int.Parse(Console.ReadLine());
+Console.WriteLine((a > b) ? (a > c) ? a : c : (b > c) ? b : c );
+Console.ReadKey();
+
---
+++ №2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Напишите программу, которая на вход принимает
+// два числа и выдает,какое число наибольшее, а какое наименьшее
+    
+ 
+
+    
+
+ int a, b;
+
+ Console.WriteLine("Введите два числа : ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+b = Convert.ToInt32(Console.ReadLine());
+
+     if (a > b)
+
+     {
+
+        Console.WriteLine("{0} наибольшее число", a);
+        Console.WriteLine("{0} наименьше число", b);
+
+     }
+
+     else
+
+     {
+
+        Console.WriteLine("{0} наибольшее число ", b);
+        Console.WriteLine("{0} наименьше число ", a);
+     }
+
+    Console.ReadLine();
+    
+
+    
