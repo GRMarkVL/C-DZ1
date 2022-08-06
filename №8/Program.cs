c51df2b66a5703e9f8c21e8f5196b8df5a47ae3a
GRMarkVL <ClimatFL@gmail.com>
2022-08-06 21:41:41 +0300

    решил задачу №8

---
+++ №8/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу , которая на вход принимает число n,
+//а на выходе показывает все чётные числа от 1 до n
+Console.Write("введите число n: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
